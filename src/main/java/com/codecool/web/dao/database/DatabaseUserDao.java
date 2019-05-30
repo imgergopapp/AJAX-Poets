e97bcc1dd33733d90c01d367fcf10ed0f08deeb4
Gergő Papp <imgergopapp@gmail.com>
2019-05-30 17:27:23 +0200

    Changed sql query & fetch methods

--- src/main/java/com/codecool/web/dao/database/DatabaseUserDao.java
+++ src/main/java/com/codecool/web/dao/database/DatabaseUserDao.java
@@ -14,7 +14,7 @@ public final class DatabaseUserDao extends AbstractDao implements UserDao {
     }
 
     public List<User> findAll() throws SQLException {
-        String sql = "SELECT id, email, password FROM users";
+        String sql = "SELECT user_id, email, name password FROM users";
         try (Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(sql)) {
             List<User> users = new ArrayList<>();
@@ -30,7 +30,7 @@ public final class DatabaseUserDao extends AbstractDao implements UserDao {
         if (email == null || "".equals(email)) {
             throw new IllegalArgumentException("Email cannot be null or empty");
         }
-        String sql = "SELECT id, email, password FROM users WHERE email = ?";
+        String sql = "SELECT user_id, email, password, name FROM users WHERE email = ?";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, email);
             try (ResultSet resultSet = statement.executeQuery()) {
@@ -43,9 +43,10 @@ public final class DatabaseUserDao extends AbstractDao implements UserDao {
     }
 
     private User fetchUser(ResultSet resultSet) throws SQLException {
-        int id = resultSet.getInt("id");
+        int id = resultSet.getInt("user_id");
         String email = resultSet.getString("email");
         String password = resultSet.getString("password");
-        return new User(id, email, password);
+        String name = resultSet.getString("name");
+        return new User(id, email, password, name);
     }
 }
