dec466bb7c3fabe84a1865d43d1a9469b247ef7b
Gergő Papp <imgergopapp@gmail.com>
2019-05-30 17:32:47 +0200

    Implemented WorkServlet with doGet & doPost HTTP methods

---
+++ src/main/java/com/codecool/web/servlet/WorkServlet.java
@@ -0,0 +1,50 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.dao.WorkDao;
+import com.codecool.web.dao.database.DatabaseWorkDao;
+import com.codecool.web.model.Work;
+import com.codecool.web.service.WorkService;
+import com.codecool.web.service.simple.SimpleWorkService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet("/protected/work")
+public class WorkServlet extends AbstractServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (Connection connection = getConnection(req.getServletContext())) {
+            WorkDao workDao = new DatabaseWorkDao(connection);
+            WorkService workService = new SimpleWorkService(workDao);
+
+            int workId = Integer.parseInt(req.getParameter("workId"));
+            Work work = workService.findByWorkId(workId);
+
+            sendMessage(resp, HttpServletResponse.SC_OK, work);
+        } catch (SQLException ex) {
+            handleSqlError(resp, ex);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (Connection connection = getConnection(req.getServletContext())) {
+            WorkDao workDao = new DatabaseWorkDao(connection);
+            WorkService workService = new SimpleWorkService(workDao);
+
+            String regex = req.getParameter("regex");
+            int workId = Integer.parseInt(req.getParameter("workId"));
+            int count = workService.countRegexOccurances(regex, workId);
+
+            sendMessage(resp, HttpServletResponse.SC_OK, count);
+        } catch (SQLException ex) {
+            handleSqlError(resp, ex);
+        }
+    }
+}
